b11de2c9f5372244aaf75d1df870c3b031130220
MarcoVillanueva705 <villanuevamarco98@gmail.com>
2019-11-16 18:26:53 -0700

    Saturday 11/16/19

--- app.js
+++ app.js
@@ -4,6 +4,40 @@ let player = {
 
 let cheeseDisplay = document.querySelector('#cheesecount')
 
+let axeDisplay = document.querySelector('#axecount')
+
+let cartDisplay = document.querySelector('#cartcount')
+
+let minerDisplay = document.querySelector('#minercount')
+
+let roverDisplay = document.querySelector('#rovercount')
+
+let clickUpgrades = {
+  pickaxes: {
+    price: 100,
+    quantity: 0,
+    multiplier: 1
+  },
+  carts: {
+    price: 300,
+    quantity: 0,
+    multiplier: 5
+  },
+  miners: {
+    price: 500,
+    quantity: 0,
+    multiplier: 10
+  }
+};
+
+let automaticUpgrades = {
+  rovers: {
+    price: 600,
+    quantity: 0,
+    multiplier: 20
+  }
+};
+
 function mine() {
   player.cheese++;
   updateCheese();
@@ -13,5 +47,40 @@ function updateCheese() {
   cheeseDisplay.textContent = player.cheese.toString();
 }
 
+function buyPickaxe() {
+  if (player.cheese > clickUpgrades.pickaxes.price) {
+    player.cheese - clickUpgrades.pickaxes.price;
+    clickUpgrades.pickaxes.quantity++;
+    axeDisplay.textContent = clickUpgrades.pickaxes.quantity.toString();
+  }
+}
 
-updateCheese()
+function buyCart() {
+  if (player.cheese > clickUpgrades.carts.price) {
+    player.cheese - clickUpgrades.carts.price;
+    clickUpgrades.carts.quantity++;
+    cartDisplay.textContent = clickUpgrades.carts.quantity.toString();
+  }
+}
+
+function buyMiner() {
+  if (player.cheese > clickUpgrades.miners.price) {
+    player.cheese - clickUpgrades.miners.price;
+    clickUpgrades.miners.quantity++;
+    minerDisplay.textContent = clickUpgrades.miners.quantity.toString();
+  }
+}
+
+// function buyRover() {
+//   if (player.cheese > automaticUpgrades.rovers.price) {
+//     player.cheese - automaticUpgrades.rovers.price;
+//     automaticUpgrades.rovers.quantity++;
+//     roverDisplay.textContent = automaticUpgrades.rovers.quantity.toString();
+//   }
+// }
+
+function collectAutoUpgrades() {
+  for (const automaticUpgrades.rovers.quantity in automaticUpgrades) {
+  }
+
+  updateCheese()
